683a52aca7293e5cae7b8986c82322fb506ceca0
Alexander Yakushev <alex@bytopia.org>
2015-05-13 21:52:39 +0200

    Screw .sync()

--- src/jvm/clojure/lang/Compiler.java
+++ src/jvm/clojure/lang/Compiler.java
@@ -7276,7 +7276,7 @@ static public void writeClassFile(String internalName, byte[] bytecode) throws I
 		{
 		cfs.write(bytecode);
 		cfs.flush();
-		cfs.getFD().sync();
+		// cfs.getFD().sync();
 		}
 	finally
 		{
